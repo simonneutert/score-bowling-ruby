63726ebdf885b6511b12909e61102e05731f07ed
Simon Neutert <simonneutert@Simons-MBP.fritz.box>
2018-02-15 13:15:17 +0100

    green

--- bowling.rb
+++ bowling.rb
@@ -45,40 +45,56 @@ class Game
   def score_pins
     strike_not_in_last_frame?
     spare_or_open_frame?
-    last_frame?
+    in_last_frame?
   end
 
   def strike_not_in_last_frame?
-    if (@throws == 1) && (@pins == 10) && (@counter < 9)
+    if (@throws == 1) && (@pins == 10) && not_last_frame?
       @frame.type = :strike
       calc_frame_scores
+      return true
+    else
+      return false
     end
+  end
+
+  def not_last_frame?
+    @counter < 9
   end
 
   def spare_or_open_frame?
-    # second throw of a frame (not last frame)
-    if (@counter < 9) && (@throws == 2)
-      raise BowlingError if @scoring.last(2).inject(&:+) > 10
+    if not_last_frame? && (@throws == 2)
+      raise BowlingError if scores_of_last(2) > 10
       @frame.type = @frame.score == 10 ? :spare : :open
       calc_frame_scores
+      return true
+    else
+      return false
     end
   end
 
-  def last_frame?
-    return if @counter != 9
-    spare_or_open_frame_in_last_frame?
-    bonus_round?
-    if (@scoring.last(2).first != 10) && (@scoring.last(2).inject(&:+) > 10) && (@scoring.last(3)[0, 2].inject(&:+) != 10)
-      raise BowlingError
+  def in_last_frame?
+    if @counter == 9
+      spare_or_open_frame_in_last_frame?
+      bonus_round?
+      if (first_of_current_frame != 10) && (scores_of_last(2) > 10) && (@scoring.last(3)[0, 2].inject(&:+) != 10)
+        raise BowlingError
+      end
+      return true
+    else
+      return false
     end
   end
 
   def spare_or_open_frame_in_last_frame?
-    if (@throws == 2) && (@scoring.last(2).inject(&:+) < 10)
+    if (@throws == 2) && (scores_of_last(2) < 10)
       @frame.type = :open
       calc_frame_scores
       @score += @scoring.last(2).inject(&:+)
       @status = :closed
+      return true
+    else
+      return false
     end
   end
 
@@ -88,38 +104,56 @@ class Game
         calc_frame_scores
         @score += @scoring.last
       else
-        @score += @scoring.last(3).inject(&:+)
+        @score += scores_of_last(3)
       end
       @frame.type = :end
       @status = :closed
+      return true
+    else
+      return false
     end
   end
 
+
   def two_past_frames_are_strikes?
-    if (@counter - 2 >= 0) && (@frames[@counter - 1].type == :strike) && (@frames[@counter - 2].type == :strike)
-      @score += if @frame.type == :strike
-                  (10 + 10 + 10)
-                else
-                  (10 + 10 + @scoring.last(2).first)
-                end
+    if game_has_previous_frames?(2) && (@frames[@counter - 1].type == :strike) && (@frames[@counter - 2].type == :strike)
+      if @frame.type == :strike
+        @score += (10 + 10 + 10)
+      else
+        @score += (10 + 10 + first_of_current_frame)
+      end
+      return true
+    else
+      return false
     end
   end
 
   def open_or_spare_and_strike_previous?
-    # no strike frame but previous frame was a strike
-    if (@counter - 1 >= 0) && (@frames[@counter - 1].type == :strike) && (@frame.type != :strike)
-      @score += (10 + @scoring.last(2).inject(&:+) * 2)
+    if game_has_previous_frames? && (@frames[@counter - 1].type == :strike) && (@frame.type != :strike)
+      @score += (10 + scores_of_last(2) * 2)
+      return true
+    else
+      return false
     end
   end
 
   def past_frame_spare?
     # past frame was a spare
-    if (@throws == 2) && (@counter - 1 >= 0) && (@frames[@counter - 1].type == :spare)
-      @score += if @pins == 0
-                  (10 + @scoring.last(2).inject(&:+) * 2)
-                else
-                  (10 + @scoring.last(2).first)
-                end
+    if (@throws == 2) && game_has_previous_frames? && (@frames[@counter - 1].type == :spare)
+      @score += @pins == 0 ? (10 + scores_of_last(2) * 2) : (10 + first_of_current_frame)
+      return true
+    else
+      return false
+    end
+  end
+
+  def past_frame_was_open?
+    # past frame was open
+    if (@throws == 2) && (@frames[@counter - 1].type == :open)
+      @score += scores_of_last(2)
+      return true
+    else
+      return false
     end
   end
 
@@ -127,15 +161,31 @@ class Game
     two_past_frames_are_strikes?
     open_or_spare_and_strike_previous?
     past_frame_spare?
+    past_frame_was_open?
+    close_frame
+  end
 
-    # past frame was open
-    if (@throws == 2) && (@frames[@counter - 1].type == :open)
-      @score += @scoring.last(2).inject(&:+)
-    end
-
+  def close_frame
     @throws = 0
     @counter += 1
   end
+
+  def scores_of_last(x)
+    if @scoring.size >= x
+      @scoring.last(x).inject(&:+)
+    else
+      raise BowlingError
+    end
+  end
+
+  def first_of_current_frame
+    @scoring.last(2).first
+  end
+
+  def game_has_previous_frames?(x=1)
+    @counter - x >= 0
+  end
+
 end
 
 class Frame
